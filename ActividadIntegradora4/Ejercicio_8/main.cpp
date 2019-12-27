516a5a7a3d4444515a173104bbfe2f04f5e1b011
Jose Alberto alapizco Figueroa <jalapizco@coppel.com>
2019-12-27 16:53:14 -0700

    Avance E8 bloque4

--- ActividadIntegradora4/Ejercicio_8/main.cpp
+++ ActividadIntegradora4/Ejercicio_8/main.cpp
@@ -1,27 +1,95 @@
 #include <iostream>
-#include<bits/stdc++.h>
 
 using namespace std;
 
 class Fecha{
 public:
-    bool ValidaFecha(int mes);
-    bool ValidaFecha(string mes);
+    Fecha( int anio, int mes, int dia, int hora );
+    Fecha( int anio, string mes, int dia, int hora );
+    bool ValidaFechaInt();
+    bool ValidaFechaStr();
+    string getMensajeError();
+    bool hayError;
+    int getAnio();
+    int getMes();
+    string getSMes();
+    int getDia();
+    int getHora();
+    string RegresaFechaCompleta();
 private:
     int anio,
         mes,
         dia,
         hora;
+    string sMes;
     string msjError;
 };
 
-bool Fecha::ValidaFecha(int mes){
+Fecha::Fecha( int anio, int mes, int dia, int hora ){
+
+    this->anio = anio;
+    this->mes = mes;
+    this->dia = dia;
+    this->hora = hora;
+
+    this->hayError = !this->ValidaFechaInt();
+
+    if( this->hayError ){
+        this->anio = 0;
+        this->mes = 0;
+        this->dia = 0;
+        this->hora = 0;
+    }
+}
+
+Fecha::Fecha( int anio, string sMes, int dia, int hora ){
+
+    this->anio = anio;
+    this->sMes = sMes;
+    this->dia = dia;
+    this->hora = hora;
+
+    this->hayError = !this->ValidaFechaStr();
+
+    if( this->hayError ){
+        this->anio = 0;
+        this->sMes = "";
+        this->dia = 0;
+        this->hora = 0;
+    }
+}
+
+string Fecha::getMensajeError(){
+    return this->msjError;
+}
+
+int Fecha::getAnio(){
+    return this->anio;
+}
+
+int Fecha::getMes(){
+    return this->mes;
+}
+
+string Fecha::getSMes(){
+    return this->sMes;
+}
+
+int Fecha::getDia(){
+    return this->dia;
+}
+
+int Fecha::getHora(){
+    return this->hora;
+}
+
+bool Fecha::ValidaFechaInt(){
     bool todoBien = true;
     this->msjError = "";
     int num_mes[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     int dias_mes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if(mes > 12 || mes < 1){
+    if(this->mes > 12 || this->mes < 1){
         this->msjError = "Formato de mes incorrecto.\n";
         todoBien = false;
     }
@@ -36,29 +104,39 @@ bool Fecha::ValidaFecha(int mes){
         todoBien = false;
     }
 
-    if(dias_mes[mes - 1] >= this->dia){
-        if(mes == 2 && this->dia == 29 && this->anio % 4 == 0){
+    if(dias_mes[mes - 1] < this->dia){
+        if(this->mes == 2 && this->dia == 29 && this->anio % 4 == 0){
 
         }else{
             this->msjError += "Excede los días del mes.\n";
             todoBien = false;
         }
     }
-
+    return todoBien;
 }
-/*
-bool Fecha::ValidaFecha(string mes){
-     bool todoBien = true;
+
+bool Fecha::ValidaFechaStr(){
+    bool todoBien = true;
     this->msjError = "";
-    string num_mes[] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+    int num_mes[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    string nom_mes[] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
     int dias_mes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int conta = 0,
+        numMes = 0;
 
-    for(int i = 0; i < sizeof(num_mes); i++){
-        if(num_mes[i] == mes :: tolower)
+    for(int i = 0; i < 12 ; i++){
+        if( this->sMes == nom_mes[i] )
+        {
+            conta++;
+            numMes = i;
+            this->mes = conta;
+            break;
+        }
     }
 
-    if(mes > 12 || mes < 1){
-        this->msjError = "Formato de mes incorrecto.\n";
+    // validamos que exista el mes
+    if( conta == 0 ){
+        this->msjError = "El mes " + this->sMes + ", es incorrecto, favor de validar.\n";
         todoBien = false;
     }
 
@@ -72,38 +150,154 @@ bool Fecha::ValidaFecha(string mes){
         todoBien = false;
     }
 
-    if(dias_mes[mes - 1] >= this->dia){
-        if(mes == 2 && this->dia == 29 && this->anio % 4 == 0){
+    if(dias_mes[ numMes ] < this->dia){
+        if(this->sMes == "febrero" && this->dia == 29 && this->anio % 4 == 0){
 
         }else{
             this->msjError += "Excede los días del mes.\n";
             todoBien = false;
         }
     }
+    return todoBien;
 }
-*/
+
+string Fecha::RegresaFechaCompleta(){
+    return std::to_string( this->getAnio() ) + "-" + std::to_string( this->getMes() ) + "-" + std::to_string( this->getDia() ) + ", " + std::to_string( this->getHora() ) + " horas.";
+}
 
 class Cita{
 public:
+    void setNombre_1( string nombre_1 );
+    string getNombre_1();
+    void setNombre_2( string nombre_2 );
+    string getNombre_2();
+    void setLugarDeReunion( string lugarDeReunion );
+    string getLugarDeReunion();
+    void setFechaCita( Fecha* fecha );
+    Fecha* getFechaCita();
 private:
     string nombre_1,
            nombre_2,
            lugarDeReunion;
+    Fecha* fecha_cita;
 
 };
 
+void Cita::setNombre_1( string nombre_1 ){
+    this->nombre_1 = nombre_1;
+}
+
+string Cita::getNombre_1(){
+    return this->nombre_1;
+}
+
+void Cita::setNombre_2( string nombre_2 ){
+    this->nombre_2 = nombre_2;
+}
+
+string Cita::getNombre_2(){
+    return this->nombre_2;
+}
+
+void Cita::setLugarDeReunion( string lugarDeReunion ){
+    this->lugarDeReunion = lugarDeReunion;
+}
+
+string Cita::getLugarDeReunion(){
+    return this->lugarDeReunion;
+}
+
+void Cita::setFechaCita( Fecha* fecha ){
+    this->fecha_cita = fecha;
+}
+
+Fecha* Cita::getFechaCita(){
+    return this->fecha_cita;
+}
 
 class ClaseExterna{
 public:
     ClaseExterna();
+    bool CrearCita( string nombre_1, string nombre_2, string lugarDeReunion, int anio, int mes, int dia, int hora );
+    bool CrearCita( string nombre_1, string nombre_2, string lugarDeReunion, int anio, string sMes, int dia, int hora  );
+    void ConsultarCitas();
 private:
     Cita* citas;
+    int indice;
 };
 
 ClaseExterna::ClaseExterna(){
-    this->citas = new Citas[10];
+    this->citas = new Cita[10];
+    this->indice = 0;
 }
 
+bool ClaseExterna::CrearCita( string nombre_1, string nombre_2, string lugarDeReunion, int anio, int mes, int dia, int hora  ){
+    bool resp = true;
+    if( this->indice < 10 ){
+        this->citas[ this->indice ].setNombre_1( nombre_1 );
+        this->citas[ this->indice ].setNombre_2( nombre_2 );
+        this->citas[ this->indice ].setLugarDeReunion( lugarDeReunion );
+        this->citas[ this->indice ].setFechaCita( new Fecha( anio, mes, dia, hora ));
+
+        if( this->citas[ this->indice ].getFechaCita()->hayError ){
+            resp = false;
+            this->citas[ this->indice ].setNombre_1( "" );
+            this->citas[ this->indice ].setNombre_2( "" );
+            this->citas[ this->indice ].setLugarDeReunion( "" );
+            cout << "No se registro la cita por error en la fecha" << endl;
+        }else
+        {
+            cout << "Se registro correctamente la cita";
+            this->indice++;
+        }
+    }else{
+        cout << "No se puede registrar la cita debio a que se alcanzo el maximo de citas a registrar.";
+        resp = false;
+    }
+    return resp;
+}
+
+bool ClaseExterna::CrearCita( string nombre_1, string nombre_2, string lugarDeReunion, int anio, string sMes, int dia, int hora  ){
+    bool resp = true;
+    if( this->indice < 10 ){
+        this->citas[ this->indice ].setNombre_1( nombre_1 );
+        this->citas[ this->indice ].setNombre_2( nombre_2 );
+        this->citas[ this->indice ].setLugarDeReunion( lugarDeReunion );
+        this->citas[ this->indice ].setFechaCita( new Fecha( anio, sMes, dia, hora ));
+
+        if( this->citas[ this->indice ].getFechaCita()->hayError ){
+            resp = false;
+            this->citas[ this->indice ].setNombre_1( "" );
+            this->citas[ this->indice ].setNombre_2( "" );
+            this->citas[ this->indice ].setLugarDeReunion( "" );
+            cout << "No se registro la cita por error en la fecha" << endl;
+        }else
+        {
+            cout << "Se registro correctamente la cita" << endl;
+            this->indice++;
+        }
+    }else{
+        cout << "No se puede registrar la cita debio a que se alcanzo el maximo de citas a registrar.";
+        resp = false;
+    }
+    return resp;
+}
+
+void ClaseExterna::ConsultarCitas(){
+    cout << endl;
+    cout << "=====================================" << endl;
+    cout << "= C I T A S   R E G I S T R A D A S =" << endl;
+    cout << "=====================================" << endl;
+    cout << endl;
+    for(int i = 0; i < this->indice; i++){
+        cout << "Cita #: " << i + 1 << endl;
+        cout << "- Persona 01: " << this->citas[i].getNombre_1() << endl;
+        cout << "- Persona 02: " << this->citas[i].getNombre_2() << endl;
+        cout << "- Lugar de la reunión: " << this->citas[i].getLugarDeReunion() << endl;
+        cout << "> Fecha: " << this->citas[i].getFechaCita()->RegresaFechaCompleta() << endl;
+        cout << endl;
+    }
+}
 
 int main()
 {
@@ -131,6 +325,40 @@ int main()
             > crear citas e ir agregandolas a la lista de citas
             > y también visualizar las citas que ya se han agendado.
     */
+
+    setlocale(LC_ALL, "");
+
+    Fecha fecha( 2020, 3, 31, 18 );
+
+    if( fecha.getMensajeError() != "" ){
+        cout << fecha.getMensajeError() << endl;
+    }else{
+        cout << "Año: " << fecha.getAnio() << endl;
+        cout << "Mes: " << fecha.getMes() << endl;
+        cout << "Día: " << fecha.getDia() << endl;
+        cout << "Hora: " << fecha.getHora() << endl;
+    }
+
+    cout << endl;
+
+    Fecha fecha2( 2020, "enero", 31, 5 );
+
+    if( fecha2.getMensajeError() != "" ){
+        cout << fecha2.getMensajeError() << endl;
+    }else{
+        cout << "Año: " << fecha2.getAnio() << endl;
+        cout << "Mes: " << fecha2.getSMes() << endl;
+        cout << "Día: " << fecha2.getDia() << endl;
+        cout << "Hora: " << fecha2.getHora() << endl;
+    }
+
+    ClaseExterna ce;
+
+    ce.CrearCita("Alberto", "Denisse", "Forum", 2020, 2, 29, 14);
+    ce.CrearCita("Mario", "Cruz Ernesto", "La Primavera", 2019, 12, 28, 12);
+    ce.CrearCita("Yolanda", "Jesus Alberto", "Sala B-2", 2019, "diciembre", 31, 11);
+    ce.ConsultarCitas();
+
 
     return 0;
 }
